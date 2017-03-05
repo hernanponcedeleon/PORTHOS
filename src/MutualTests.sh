6b27abd6d935ae7daaa5f48fcaa11dd9491337be
Zeta <ponceh1@t30500-la058.org.aalto.fi>
2017-03-06 00:05:37 +0100

    Scripts

--- src/MutualTests.sh
+++ src/MutualTests.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 for file in ../benchmarks/*.pts; do echo $file; ./porthos.py -s tso -t power -i $file; done
--- src/PowerTests.sh
+++ src/PowerTests.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 for file in ../litmus/*.litmus; do echo $file; ./porthos.py -s tso -t power -i $file; done
